053ad526211a1dd4bccab633dd527e850a2e00a2
omarsalim <83906211+Everezze@users.noreply.github.com>
2025-09-16 01:50:32 +0200

    rewrote numbers initial state, checking all elements are clicked

--- App.tsx
+++ App.tsx
@@ -6,21 +6,28 @@ import React from "react"
 export default function App(){
 	console.log("drawing app");
 	let [firstGame,setFirstGame] = React.useState(true);
-	let [numbers,setNumbers] = React.useState([]);
-	let [isWon, setIsWon] = React.useState(false);
-
-
-	if(!numbers.length){
+	let [numbers,setNumbers] = React.useState(()=>{
 		let tempArray = [];
 		for(let i =0;i<10;i++){
 			let randomInt = Math.floor(Math.random() * 6) + 1;
 			tempArray.push({num:randomInt,frozen:false});
 		}
-		setNumbers(tempArray);
-	}
+		return tempArray;
+	});
+	let [isWon, setIsWon] = React.useState(false);
+	const numOfFrozen = numbers.filter(numObj => numObj.frozen).length;
+
+	if(numbers.length){
+		if(!isWon && numOfFrozen == numbers.length){
+			setIsWon(true);
+			return;
+		}
+	};
 
 	function toggleActive(e){
 		const targetIndex = e.currentTarget.dataset.id;
+		console.log(numOfFrozen, "==?",numbers.length);
+		console.log("equal or not:",numOfFrozen == numbers.length);
 		setNumbers(
 			numbers.map((numObj,index)=> {
 				if(index == targetIndex){
@@ -31,34 +38,22 @@ export default function App(){
 		);
 	};
 
-	function checkWin(){
-		let targetNum = null;
-		return numbers.every(numObj => {
-			if(!targetNum){
-				targetNum = numObj.num;
-			}
-			return numObj.frozen && numObj.num == targetNum;
-		});
-	}
-
 	function generateNewNumbers(){
-		console.log("gen new nums running...");
 		setNumbers(prevNumbers => {
-			console.log("prev nums: ",prevNumbers);
 			const tempNumbers = prevNumbers.map(numObj => {
 				if(!numObj.frozen){
 					let randomInt = Math.floor(Math.random() * 6) + 1;
 					return {...numObj,num:randomInt};
 				}
-				return numObj
+				return numObj;
 			})
-			console.log("temp nums is: ",tempNumbers);
 			return tempNumbers;
 		})
 	}
 
 	function restartGame(){
 		setNumbers([]);
+		setIsWon(false);
 	}
 
 	return (
